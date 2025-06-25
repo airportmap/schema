87189f01ba2d20f2609be9dc8fe3eb936ab3448a
komed3 <pk97@outlook.de>
2025-06-25 19:55:20 +0200

    some changes + `_options` field added

--- schema/user.sql
+++ schema/user.sql
@@ -1,15 +1,17 @@
 CREATE TABLE user (
 
-    _id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    _id INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 
-    username VARCHAR( 32 ) NOT NULL UNIQUE,
-    email VARCHAR( 255 ) NOT NULL UNIQUE,
-    passwd_hash VARCHAR( 255 ) NOT NULL,
-    token VARCHAR( 255 ) NULL,
+    username VARBINARY( 32 ) NOT NULL UNIQUE,
+    email VARBINARY( 255 ) NOT NULL UNIQUE,
+    passwd_hash TINYBLOB NOT NULL,
+    passwd_newhash TINYBLOB NULL,
+    token BINARY( 32 ) NULL,
+    token_expires DATETIME NULL,
 
-    _verified DATETIME NULL,
     _role ENUM( 'user', 'moderator', 'admin', 'bot' ) NOT NULL DEFAULT 'user',
     _status ENUM( 'pending', 'active', 'banned', 'suspended', 'deleted' ) NOT NULL DEFAULT 'pending',
+    _verified DATETIME NULL,
 
     registered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     activated DATETIME NULL,
@@ -18,13 +20,16 @@ CREATE TABLE user (
     failed_attempts INT UNSIGNED NOT NULL DEFAULT 0,
     last_failed DATETIME NULL,
 
-    _2fa_secret VARCHAR( 255 ) NULL,
-    _2fa_created DATETIME NULL,
-
     edit_count INT UNSIGNED NOT NULL DEFAULT 0,
+    pending_edits INT UNSIGNED NOT NULL DEFAULT 0,
     approved_edits INT UNSIGNED NOT NULL DEFAULT 0,
     rejected_edits INT UNSIGNED NOT NULL DEFAULT 0,
     last_edit DATETIME NULL,
+
+    _2fa_secret VARBINARY( 255 ) NULL,
+    _2fa_created DATETIME NULL,
+
+    _options JSON NULL,
 
     KEY user_role ( _role ),
     KEY account_status ( _status )
