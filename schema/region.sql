ade841599dab6dcccb622d06154f298edb25a62b
komed3 <pk97@outlook.de>
2025-06-25 23:59:46 +0200

    +2 tables (empty)

---
+++ schema/region.sql
@@ -0,0 +1,9 @@
+-- ========================================================================
+-- TABLE region
+-- ------------------------------------------------------------------------
+-- Stores global geographic units including continents, countries,
+-- and administrative subregions. Hierarchical with ISO codes and
+-- optional multilingual labels.
+-- ========================================================================
+
+CREATE TABLE region ();
