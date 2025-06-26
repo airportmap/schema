1dffa7127e276b20906e3a7074b6bf474185eb06
komed3 <pk97@outlook.de>
2025-06-27 00:31:04 +0200

    create the `wiki` table

---
+++ schema/wiki.sql
@@ -0,0 +1,34 @@
+-- ========================================================================
+-- TABLE: wiki
+-- ------------------------------------------------------------------------
+-- Holds introductory texts (usually from Wikipedia) per airport,
+-- keyed by language.
+--
+-- This table will be populated automatically.
+-- ========================================================================
+
+CREATE TABLE wiki (
+
+    -- Internal ID
+    _id INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+    -- Referenced airport ID
+    airport INT( 10 ) UNSIGNED NOT NULL,
+
+    -- Language code
+    lang VARBINARY( 8 ) NOT NULL,
+
+    -- The lead paragraph / summary
+    content BLOB NOT NULL,
+
+    -- Last update timestamp
+    _touched DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+
+    -- Indexes
+    UNIQUE KEY wiki_lang ( airport, lang ),
+
+    -- Foreign key constraints
+    FOREIGN KEY ( airport ) REFERENCES airport ( _id )
+
+);
